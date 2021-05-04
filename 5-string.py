a7db527f397eba5612cee766d5a5f906098b9c1d
ibrohimoff <82875468+ibrohimoff@users.noreply.github.com>
2021-05-04 12:29:23 +0500

    Create 5-string.py

---
+++ 5-string.py
@@ -0,0 +1,57 @@
+#ism = 'Ahad'
+#familiya = 'Qayum'
+#print(ism+familiya)
+#print("Men", ism+' '+familiya,"man")
+#ism_sharif = f"{ism} {familiya}"
+#print('Mening ismim',ism_sharif.lower())
+
+#ism = 'James'
+#familiya = 'Bond'
+#print(f"Mening ismim {familiya}. {ism} {familiya}")
+
+#print('Hello world!')
+#print('Hello \tworld!')
+#print('Hello \nworld!')
+
+#ism_sharif = 'james bond'
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+
+#meva = '    olma     '
+#print("Men "+ meva.lstrip()+"ni yaxshi ko'raman")
+#print("Men "+ meva.rstrip()+"ni yaxshi ko'raman")
+#print("Men "+ meva.strip()+"ni yaxshi ko'raman")
+
+#ism = input('Ismingiz nima?\n>>>')
+#print('Assalomu alaykum, '+ ism.title())
+
+kocha = input('Ko\'changizni kiriting : ')
+mahalla = input('Mahallangiz : ')
+tuman = input('Yashaydigan tuman : ')
+viloyat = input('Viloyatingiz : ')
+print(  kocha.title() + " ko'chasi\n "+ mahalla.title() +" mahallasi\n "+ tuman.title() +" tumani\n "+
+      viloyat.title() +" viloyati. ")
+
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil.lower())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
